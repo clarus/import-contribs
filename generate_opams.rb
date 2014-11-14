672761a3d717a0cd8cec581d6011f21cd0c6fb56
Guillaume Claret <dev@clarus.me>
2014-11-14 14:32:36 +0100

    Packages for .dev and .8.4.dev

--- generate_opams.rb
+++ generate_opams.rb
@@ -6,30 +6,33 @@ require 'erb'
 contribs = Dir.glob("gits/*").map {|name| File.basename(name)}.sort
 
 versions = {
-  "8.4.dev" => "8.4",
-  "dev" => "master"
+  "8.4.dev" => [">= \"8.4\" & < \"8.5\"", "v8.4"],
+  "dev" => ["= \"dev\"", "trunk"]
 }
 
 # Create the packages.
 system("mkdir -p packages")
 for contrib in contribs do
   puts contrib
-  path = "packages/coq:contrib:#{contrib}/coq:contrib:#{contrib}.dev"
-  system("mkdir -p #{path}")
-  # `descr`
-  description = File.read("gits/#{contrib}/description", encoding: "BINARY")
-  descr = description.match(/Title\s*\:(.*)$/)[1].strip
-  descr += "." if descr[-1] != "."
-  File.open("#{path}/descr", "w") do |file|
-    file << descr
-  end
-  # `opam`
-  renderer = ERB.new(File.read("opam.erb", encoding: "UTF-8"))
-  File.open("#{path}/opam", "w") do |file|
-    file << renderer.result()
-  end
-  # `url`
-  File.open("#{path}/url", "w") do |file|
-    file << "git: \"git://clarus.io/#{contrib}.git\""
+  for version, info in versions do
+    coq, branch = info
+    path = "packages/coq:contrib:#{contrib}/coq:contrib:#{contrib}.#{version}"
+    system("mkdir -p #{path}")
+    # `descr`
+    description = File.read("gits/#{contrib}/description", encoding: "BINARY")
+    descr = description.match(/Title\s*\:(.*)$/)[1].strip
+    descr += "." if descr[-1] != "."
+    File.open("#{path}/descr", "w") do |file|
+      file << descr
+    end
+    # `opam`
+    renderer = ERB.new(File.read("opam.erb", encoding: "UTF-8"))
+    File.open("#{path}/opam", "w") do |file|
+      file << renderer.result()
+    end
+    # `url`
+    File.open("#{path}/url", "w") do |file|
+      file << "git: \"git://clarus.io/#{contrib}\##{branch}\""
+    end
   end
 end
