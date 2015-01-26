19ea9906a48c696514fef1bd47df2948a50449a7
Guillaume Claret <dev@clarus.me>
2015-01-26 14:04:44 +0100

    More cleaning

--- clean_svn.rb
+++ clean_svn.rb
@@ -1,9 +1,18 @@
-# Remove the `.git/svn` folders.
-
 # Get the list of contribs.
 contribs = Dir.glob("gits/*").map {|name| File.basename(name)}.sort
 
+# Remove the `.git/svn` folders.
 for contrib in contribs do
   puts contrib
   system("rm -Rf gits/#{contrib}/.git/svn")
+end
+
+# Import the branches from `origin/*` to `*`.
+for contrib in contribs do
+  for branch in `cd gits/#{contrib} && git branch`.split("\n") do
+    if /origin\/(.*)/.match(branch) then
+      system("cd gits/#{contrib} && git branch #{$1} #{branch}")
+      system("cd gits/#{contrib} && git branch -D #{branch}")
+    end
+  end
 end
